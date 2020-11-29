3b5e65dab750544c07b19c5757cd211ea57800fb
TOLBAR <tolbarweb@gmail.com>
2020-11-29 09:27:42 -0500

    ajuste cors

--- src/main/java/com/ceiba/laboratorio/config/AllowCors.java
+++ src/main/java/com/ceiba/laboratorio/config/AllowCors.java
@@ -29,11 +29,12 @@ public class AllowCors implements Filter {
 		HttpServletResponse response = (HttpServletResponse) res;
 		HttpServletRequest request = (HttpServletRequest) req;
 
-		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
 		response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers",
 				"x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN");
+		response.setHeader("Access-Control-Allow-Credentials", "true");
 
 		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
 			response.setStatus(HttpServletResponse.SC_OK);
